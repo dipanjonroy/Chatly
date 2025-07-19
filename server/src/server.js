ff942ee9cabcc60f4468ad484071dc547c3bca06
Dipanjon <dipanjon95@gmail.com>
2025-07-20 02:35:15 +0600

    Vercel update

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3030;
 
 const server = http.createServer(app);
 
-server.listen(port, ()=>{
-  console.log(`Server is listening from port ${port}`)
-})
+if (process.env.NODE_ENV !== "production") {
+  server.listen(port, () => {
+    console.log(`Server is listening from port ${port}`);
+  });
+}
