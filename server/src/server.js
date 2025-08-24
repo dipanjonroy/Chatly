d6f0c119e6496f604c54b3afaa37a1b26f1d73d4
Dipanjon <dipanjon95@gmail.com>
2025-08-24 22:43:26 +0600

    CORS Updated

--- server/src/server.js
+++ server/src/server.js
@@ -7,8 +7,12 @@ const server = http.createServer(app);
 
 connectToSocket(server);
 
-if (process.env.NODE_ENV !== "production") {
-  server.listen(port, () => {
-    console.log(`Server is listening from port ${port}`);
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   server.listen(port, () => {
+//     console.log(`Server is listening from port ${port}`);
+//   });
+// }
+
+server.listen(port, () => {
+  console.log(`✅ Server is listening on http://localhost:${port}`);
+});
